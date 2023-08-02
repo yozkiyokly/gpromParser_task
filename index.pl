d933b724b8a6990c23697571d10ed6ab3fce0fc0
yozkiyokly <yozki@yandex.ru>
2023-08-03 04:35:55 +0700

    Добавлена заготовка меню для аналитика. Из таблиц выводятся базовые сведеия о кол-ве записей, От наличия записей в буфер-таблице теперь зависит вывод поисковой строки

--- index.pl
+++ index.pl
@@ -16,8 +16,8 @@ print printHeader("Yozki perl maillog parser with searchFiltering");
 # - либо зеркальнуть таблицу 'message';       *структура отличается: там нет пропарсенных email!!
 # - либо отпарсить файл лога заново.
 $countLog=checkLogTable($dbh);
-if ($countLog){print " ...в логе $countLog записей.  ";}else{print " в логу пусто";
 
+/* ф-ция восстановления буфера после падения ДБ начнётся с этого запроса:
 # Красивое regexp_SUBSTR в один запрос предлагает Mysql,  начиная с версии 8, 
 # так ведь не везде есть восьмёрка! не говоря о десятке.
 # ...3-aug-2023... Проставил-таки восьмёрку у себя на хостинге в spaceWEB. 
@@ -29,17 +29,19 @@ SELECT
 FROM
 	`message`;";
 # Когда я уже заведу себе человеческий VDS и перестану страдать?
-}
+*/
 
 $search = param("search"); #  Надо убрать. Эту переменную потом используем целый один раз.
-searchForm($search);
+if ($countLog){searchForm($search);}
+else{print " Аналитическая таблица пуста. <br>Восстановите предыдущую загрузку или обработайте новый фапйл лога.";}
+
 
 #Давайте дадим возможность загрузить лог на сервер
 my $filename = './out';
 
 
 # Отображаем результаты поиска строк с заданным адресом:
-if($search){ searchResultsPrint($search,$dbh);	}
+if($countLog&&$search){ searchResultsPrint($search,$dbh);	}
 
 $dbh->disconnect; 
 printFooter();
